c58c2e71a38af69e049e3898022ac54aa00b2240
subash kc <subaazkc@gmail.com>
2021-12-13 09:32:52 +0545

    delete array index

--- array_insert.c
+++ array_insert.c
@@ -1,41 +1,40 @@
-#include <stdio.h>
+// Inserting and Appending in a Array 
+#include<stdio.h>
 struct Array
 {
     int A[10];
     int size;
     int length;
 };
-
-void display(struct Array arr)
+void Display(struct Array arr)
 {
     int i;
-    printf("\n Elements are\n");
+    printf("\nElements are\n");
     for (i = 0; i < arr.length; i++)
         printf("%d ", arr.A[i]);
-};
-
-// void append(struct Array *arr, int x)
-// {
-//     if (arr->length < arr->size)
-//         arr->A[arr->length++] = x;
-// }
-
-void insert(struct Array *arr, int index, int x)
+}
+void Append(struct Array *arr, int x)
+{
+    if (arr->length < arr->size)
+        arr->A[arr->length++] = x;
+}
+void Insert(struct Array *arr, int index, int x)
 {
     int i;
     if (index >= 0 && index <= arr->length)
-        arr->A[i] = arr->A[i - 1];
-    arr->A[index] = x;
-    arr->length++;
+    {
+        for (i = arr->length; i > index; i--)
+            arr->A[i] = arr->A[i - 1];
+        arr->A[index] = x;
+        arr->length++;
+    }
 }
-
 int main()
 {
-    struct Array arr1 = {{2, 3, 4, 5, 6,7},10,6};
-    // append(&arr1, 30);
-
-    insert(&arr1, 5,10);
-    display(arr1);
-    // free(arr1);
+    struct Array arr1 = {{2, 3, 4, 5, 6}, 10, 5};
+    Display(arr1);
+    Append(&arr1, 10);
+    Insert(&arr1, 0, 12);
+    Display(arr1);
     return 0;
 }
